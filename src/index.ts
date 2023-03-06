06ab36d7f5d79d1fd114330167318d930307cb57
Víctor Andújar <victorandujarmurcia@gmail.com>
2023-03-06 21:37:27 +0100

    Set up startServer

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-const a = 3;
+import "./loadEnvironment.js";
+import createDebug from "debug";
+import mongoose from "mongoose";
+import chalk from "chalk";
+import startServer from "./server/startServer.js";
+
+const port = process.env.PORT ?? 4000;
+
+const debug = createDebug("index:*");
+
+mongoose.set("toJSON", {
+  virtuals: true,
+  transform(doc, ret) {
+    delete ret._id;
+    delete ret.__v;
+  },
+});
+
+try {
+  await startServer(+port);
+  debug(chalk.green(`Server listening on port ${port}`));
+} catch (error) {
+  debug(error.message);
+}
