2e74ea79794270750a97e3b7d62ea3522797f6df
Víctor Andújar <victorandujarmurcia@gmail.com>
2023-03-07 11:42:44 +0100

    Create errorMiddlewares to manage project errors

---
+++ src/server/middlewares/errorMiddlewares/errorMiddlewares.ts
@@ -0,0 +1,29 @@
+import { type NextFunction, type Request, type Response } from "express";
+import { CustomError } from "../../../CustomError/CustomError";
+import createDebug from "debug";
+
+const debug = createDebug("bikemeet:server:middlewares:errorMiddlewares");
+
+export const notFoundError = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const error = new CustomError("Path not found", 404, "Endpoint not found");
+
+  next(error);
+};
+
+export const generalError = (
+  error: CustomError,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  debug(error.message);
+
+  const statusCode = error.statusCode || 500;
+  const publicMessage = error.publicMessage || "Something went wrong";
+
+  res.status(statusCode).json({ error: publicMessage });
+};
