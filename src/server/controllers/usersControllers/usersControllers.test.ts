19d1e2e69531780ecbafac02a1e711c64c05600e
Víctor Andújar <victorandujarmurcia@gmail.com>
2023-03-07 17:37:52 +0100

    Add test for loginUser function

---
+++ src/server/controllers/usersControllers/usersControllers.test.ts
@@ -0,0 +1,94 @@
+import { type NextFunction, type Request, type Response } from "express";
+import mongoose from "mongoose";
+import { User } from "../../../database/models/User";
+import { type UserCredentials } from "../../../types/types";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+import loginUser from "./usersControllers";
+import { CustomError } from "../../../CustomError/CustomError";
+
+const res = {
+  status: jest.fn().mockReturnThis(),
+  json: jest.fn(),
+} as Partial<Response>;
+const req = {} as Request;
+const next = jest.fn() as NextFunction;
+
+beforeEach(() => jest.clearAllMocks());
+
+const user: UserCredentials = {
+  email: "jordi@gmail.com",
+  password: "12345678",
+};
+
+describe("Given a loginUser function", () => {
+  describe("When it receives a request with an email 'jordi@gmail.com' and a password '12345678'", () => {
+    test("Then it should call its status method with 200 and its json method with a token", async () => {
+      const expectedStatus = 200;
+      req.body = user;
+      const expectedResponse = { token: "dsjakhsdsagdhgashj" };
+
+      User.findOne = jest.fn().mockImplementationOnce(() => ({
+        exec: jest.fn().mockResolvedValue({
+          ...user,
+          _id: new mongoose.Types.ObjectId(),
+        }),
+      }));
+
+      bcryptjs.compare = jest.fn().mockResolvedValue(true);
+      jwt.sign = jest.fn().mockReturnValue("dsjakhsdsagdhgashj");
+
+      await loginUser(req, res as Response, next);
+
+      expect(res.status).toHaveBeenCalledWith(expectedStatus);
+      expect(res.json).toHaveBeenCalledWith(expectedResponse);
+    });
+  });
+
+  describe("When it receives a correct email 'jordi@gmail.com' and a bad password '123232323'", () => {
+    test("Then it should respond with status 401 and a message 'Wrong credentials'", async () => {
+      const wrongUser: UserCredentials = {
+        email: "jordi@gmail.com",
+        password: "123232323",
+      };
+      req.body = wrongUser;
+      const expectedError = new CustomError(
+        "Wrong credentials",
+        401,
+        "Wrong credentials"
+      );
+
+      User.findOne = jest.fn().mockImplementationOnce(() => ({
+        exec: jest.fn().mockResolvedValue({
+          ...wrongUser,
+          _id: new mongoose.Types.ObjectId(),
+        }),
+      }));
+
+      bcryptjs.compare = jest.fn().mockResolvedValue(false);
+
+      await loginUser(req, res as Response, next);
+
+      expect(next).toHaveBeenCalledWith(expectedError);
+    });
+  });
+
+  describe("When it receives a request with an email 'jordi@gmail.com' and a password '12345678' but the user doesn't exists in the data base", () => {
+    test("Then it should call its next method with status 401 and the message 'Wrong credentials'", async () => {
+      const expectedError = new CustomError(
+        "Wrong credentials",
+        401,
+        "Wrong credentials"
+      );
+      req.body = user;
+
+      User.findOne = jest.fn().mockImplementationOnce(() => ({
+        exec: jest.fn().mockResolvedValue(undefined),
+      }));
+
+      await loginUser(req, res as Response, next);
+
+      expect(next).toHaveBeenCalledWith(expectedError);
+    });
+  });
+});
--- src/server/controllers/usersControllers/usersControllers.ts
+++ src/server/controllers/usersControllers/usersControllers.ts
@@ -24,23 +24,35 @@ const loginUser = async (
   try {
     const user = await User.findOne({ email }).exec();
 
-    const passwordConfirmation = await bcryptjs.compare(
-      password,
-      user!.password
-    );
-
-    if (!user || !passwordConfirmation) {
-      const error = new CustomError(
+    if (!user) {
+      const customError = new CustomError(
         "Wrong credentials",
         401,
         "Wrong credentials"
       );
 
-      next(error);
+      next(customError);
+      return;
+    }
+
+    const passwordConfirmation = await bcryptjs.compare(
+      password,
+      user.password
+    );
+
+    if (!passwordConfirmation) {
+      const customError = new CustomError(
+        "Wrong credentials",
+        401,
+        "Wrong credentials"
+      );
+
+      next(customError);
+      return;
     }
 
     const jwtPayload: CustomJwtPayload = {
-      sub: user!._id.toString(),
+      sub: user._id.toString(),
       email,
     };
 
