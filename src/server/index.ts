bbf35736df00b634bbfa41d0a368ecbd74261baf
Víctor Andújar <victorandujarmurcia@gmail.com>
2023-03-07 11:43:13 +0100

    Add cors dependencie to the project

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,8 +1,24 @@
+import "../loadEnvironment.js";
 import express from "express";
 import morgan from "morgan";
+import {
+  generalError,
+  notFoundError,
+} from "./middlewares/errorMiddlewares/errorMiddlewares.js";
+import cors from "cors";
 
 export const app = express();
 app.disable("x-powered-by");
 
+const allowedOrigins = [process.env.LOCALHOST!, process.env.NETLIFY_URL!];
+
+const options: cors.CorsOptions = {
+  origin: allowedOrigins,
+};
+
+app.use(cors(options));
 app.use(morgan("dev"));
 app.use(express.json());
+
+app.use(notFoundError);
+app.use(generalError);
