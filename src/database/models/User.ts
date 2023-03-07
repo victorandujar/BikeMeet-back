eb68b1b2c609055fb7e2ac4db6f766d14d5120ba
Víctor Andújar <victorandujarmurcia@gmail.com>
2023-03-07 13:17:14 +0100

    Create User Schema, its types and add express-validation schema

---
+++ src/database/models/User.ts
@@ -0,0 +1,19 @@
+import { model, Schema } from "mongoose";
+
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+});
+
+export const User = model("User", UserSchema, "users");
