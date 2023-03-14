18c956822e270532c6128d84c4d3d27bd536a872
Víctor Andújar <victorandujarmurcia@gmail.com>
2023-03-14 10:22:53 +0100

    Add testing for getUserEvents controller

--- src/server/controllers/eventsControllers/eventsControllers.test.ts
+++ src/server/controllers/eventsControllers/eventsControllers.test.ts
@@ -2,7 +2,8 @@ import { type Request, type Response } from "express";
 import { CustomError } from "../../../CustomError/CustomError";
 import { Event } from "../../../database/models/Events/Events";
 import { type EventsData, type EventData } from "../../../types/events/types";
-import { getEvents } from "./eventsControllers";
+import { type UserId } from "../../../types/users/types";
+import { getAllEvents, getUserEvents } from "./eventsControllers";
 
 const mockEventGravel: EventData = {
   name: "Sa costa",
@@ -11,6 +12,8 @@ const mockEventGravel: EventData = {
   distance: 123,
   image: "sacosta.png",
   type: "Gravel",
+  id: "",
+  postedBy: "",
 };
 
 const mockEventRoad: EventData = {
@@ -20,6 +23,8 @@ const mockEventRoad: EventData = {
   distance: 123,
   image: "sacosta.png",
   type: "Road",
+  id: "",
+  postedBy: "",
 };
 
 const mockEventsList: EventsData = [mockEventGravel, mockEventRoad];
@@ -41,12 +46,12 @@ describe("Given getEvents controller", () => {
         exec: jest.fn().mockReturnValue(mockEventsList),
       }));
 
-      await getEvents(req as Request, res as Response, next);
+      await getAllEvents(req as Request, res as Response, next);
 
       expect(res.status).toHaveBeenCalledWith(expectedStatusCode);
     });
 
-    test("Then it should call its status method", async () => {
+    test("Then it should call its json method", async () => {
       const res: Partial<Response> = {
         status: jest.fn().mockReturnThis(),
         json: jest.fn().mockResolvedValue(mockEventsList),
@@ -58,7 +63,7 @@ describe("Given getEvents controller", () => {
         exec: jest.fn().mockReturnValue(mockEventsList),
       }));
 
-      await getEvents(req as Request, res as Response, next);
+      await getAllEvents(req as Request, res as Response, next);
 
       expect(res.json).toHaveBeenCalledWith({ events: mockEventsList });
     });
@@ -83,7 +88,97 @@ describe("Given getEvents controller", () => {
 
       Event.find = jest.fn().mockReturnValue(undefined);
 
-      await getEvents(req as Request, res as Response, next);
+      await getAllEvents(req as Request, res as Response, next);
+
+      expect(next).toHaveBeenCalledWith(expectedError);
+    });
+  });
+});
+
+describe("Given the getUserEvents controller", () => {
+  describe("When it receives a response", () => {
+    test("Then it should call its status method with 200", async () => {
+      const res: Partial<Response> = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn().mockResolvedValue(mockEventsList),
+      };
+      const req: Partial<Request> = {};
+      const next = jest.fn();
+      const expectedStatusCode = 200;
+      req.body = { postedBy: "213i21309213891jkdk" };
+
+      Event.find = jest.fn().mockImplementationOnce(() => ({
+        exec: jest.fn().mockReturnValue({ postedBy: "12324324fdsvsdafsg45" }),
+      }));
+
+      await getUserEvents(
+        req as Request<
+          Record<string, unknown>,
+          Record<string, unknown>,
+          UserId
+        >,
+        res as Response,
+        next
+      );
+
+      expect(res.status).toHaveBeenCalledWith(expectedStatusCode);
+    });
+
+    test("Then it should call its json method", async () => {
+      const res: Partial<Response> = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn().mockResolvedValue(mockEventsList),
+      };
+      const req: Partial<Request> = {};
+      const next = jest.fn();
+      req.body = { postedBy: "213i21309213891jkdk" };
+
+      Event.find = jest.fn().mockImplementationOnce(() => ({
+        exec: jest.fn().mockReturnValue(mockEventsList),
+      }));
+
+      await getUserEvents(
+        req as Request<
+          Record<string, unknown>,
+          Record<string, unknown>,
+          UserId
+        >,
+        res as Response,
+        next
+      );
+
+      expect(res.json).toHaveBeenCalledWith({ events: mockEventsList });
+    });
+  });
+
+  describe("When it receives a bad request", () => {
+    test("Then it should call its next function", async () => {
+      const res: Partial<Response> = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn().mockResolvedValue({}),
+      };
+      const req: Partial<Request> = {};
+      const next = jest.fn();
+
+      const expectedError = new CustomError(
+        "Bad request",
+        400,
+        "Couldn't retrieve bike events"
+      );
+
+      req.body = {};
+
+      Event.find = jest.fn().mockReturnValue(undefined);
+
+      await getUserEvents(
+        req as Request<
+          Record<string, unknown>,
+          Record<string, unknown>,
+          UserId
+        >,
+        res as Response,
+        next
+      );
 
       expect(next).toHaveBeenCalledWith(expectedError);
     });
