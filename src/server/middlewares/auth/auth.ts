c882fb30873b077e9fc2b84b60e068e93749694a
Víctor Andújar <victorandujarmurcia@gmail.com>
2023-03-14 16:18:10 +0100

    Add auth middleware to check for user authorization

---
+++ src/server/middlewares/auth/auth.ts
@@ -0,0 +1,41 @@
+import { type Response, type NextFunction } from "express";
+import { CustomError } from "../../../CustomError/CustomError";
+import {
+  type CustomJwtPayload,
+  type CustomRequest,
+} from "../../../types/users/types";
+import jwt from "jsonwebtoken";
+
+const auth = (req: CustomRequest, res: Response, next: NextFunction) => {
+  try {
+    const authorizationHeader = req.header("Authorization");
+
+    if (!authorizationHeader) {
+      throw new Error("Authorization header missing");
+    }
+
+    if (!authorizationHeader.startsWith("Bearer ")) {
+      throw new Error("Missing bearer in Authorization header");
+    }
+
+    const token = authorizationHeader.replace(/^Bearer\s*/, "");
+
+    const { sub: postedBy } = jwt.verify(
+      token,
+      process.env.JWT_SECRET!
+    ) as CustomJwtPayload;
+
+    req.postedBy = postedBy;
+
+    next();
+  } catch (error: unknown) {
+    const tokenError = new CustomError(
+      (error as Error).message,
+      401,
+      "Invalid token"
+    );
+    next(tokenError);
+  }
+};
+
+export default auth;
