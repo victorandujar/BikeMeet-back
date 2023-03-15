dca54032815184b0a7e1b71109f592bd367db326
Víctor Andújar <victorandujarmurcia@gmail.com>
2023-03-15 17:03:13 +0100

    Add testing for deleteEvent function

--- src/server/controllers/eventsControllers/eventsControllers.test.ts
+++ src/server/controllers/eventsControllers/eventsControllers.test.ts
@@ -1,9 +1,9 @@
-import { type Request, type Response } from "express";
+import { NextFunction, type Request, type Response } from "express";
 import { CustomError } from "../../../CustomError/CustomError";
 import { Event } from "../../../database/models/Events/Events";
 import { type EventsData, type EventData } from "../../../types/events/types";
 import { type CustomRequest } from "../../../types/users/types";
-import { getAllEvents, getUserEvents } from "./eventsControllers";
+import { deleteEvents, getAllEvents, getUserEvents } from "./eventsControllers";
 
 const mockEventGravel: EventData = {
   name: "Sa costa",
@@ -155,6 +155,74 @@ describe("Given the getUserEvents controller", () => {
       Event.find = jest.fn().mockReturnValue(undefined);
 
       await getUserEvents(req as CustomRequest, res as Response, next);
+
+      expect(next).toHaveBeenCalledWith(expectedError);
+    });
+  });
+});
+
+describe("Given a deleteEvent controller", () => {
+  describe("When it receives a response", () => {
+    test("Then it should call its status method with 200", async () => {
+      const res: Partial<Response> = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn().mockResolvedValue(mockEventRoad.id),
+      };
+      const req: Partial<CustomRequest> = {
+        params: { id: `${mockEventRoad.id}` },
+      };
+      const next = jest.fn();
+      const expectedStatusCode = 200;
+
+      Event.findByIdAndDelete = jest.fn().mockImplementationOnce(() => ({
+        exec: jest.fn().mockReturnValue(mockEventRoad),
+      }));
+
+      await deleteEvents(req as CustomRequest, res as Response, next);
+
+      expect(res.status).toHaveBeenCalledWith(expectedStatusCode);
+    });
+
+    test("Then it should call its status method", async () => {
+      const res: Partial<Response> = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn().mockResolvedValue(mockEventRoad.id),
+      };
+      const req: Partial<CustomRequest> = {
+        params: { id: `${mockEventRoad.id}` },
+      };
+      const next = jest.fn();
+
+      Event.findByIdAndDelete = jest.fn().mockImplementationOnce(() => ({
+        exec: jest.fn().mockReturnValue(mockEventRoad),
+      }));
+
+      await deleteEvents(req as CustomRequest, res as Response, next);
+
+      expect(res.json).toHaveBeenCalledWith({ event: mockEventRoad });
+    });
+  });
+
+  describe("When it receives a bad request", () => {
+    test("Then it should call its next function", async () => {
+      const res: Partial<Response> = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn().mockResolvedValue({}),
+      };
+      const req: Partial<CustomRequest> = {};
+      const next = jest.fn();
+
+      req.params = {};
+
+      const expectedError = new CustomError(
+        "Internal Server Error. Sorry something went wrong.",
+        500,
+        "The event couldn't be deleted."
+      );
+
+      Event.findByIdAndDelete = jest.fn().mockReturnValue(undefined);
+
+      await deleteEvents(req as CustomRequest, res as Response, next);
 
       expect(next).toHaveBeenCalledWith(expectedError);
     });
